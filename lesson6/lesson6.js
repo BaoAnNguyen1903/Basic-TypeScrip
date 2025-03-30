903cda616c78c1a8ee15d4292296899f03ee8ac8
An Nguyen <anndb1903@gmail.com>
2025-03-30 23:16:36 +0700

    Type Annotations

---
+++ lesson6/lesson6.js
@@ -0,0 +1,11 @@
+let count = 1; //ok
+
+count = 'name'; // err ?
+
+console.log(">>>check count: ", count);
+
+let names = ['Baoan', 'dep trai', 'typescrip'] // arr just string
+names.push(25);
+
+console.log(names)
+
---
+++ lesson6/lesson6TS.ts
@@ -0,0 +1,10 @@
+let count2: number = 1; //ok
+
+count2 = 10; //err
+
+console.log(">>>check count2: ", count2);
+
+let name2 : string[] = ['Baoan', 'dep trai', 'typescrip'] // arr just string
+name2.push("100");
+
+console.log(">>>check name2: ", name2);
