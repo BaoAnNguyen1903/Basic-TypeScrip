ea12ef9d04659312df1a373f6e0c9ba734370f6a
An Nguyen <anndb1903@gmail.com>
2025-03-31 13:33:34 +0700

    Number Type

---
+++ lesson8/lesson8JS.js
@@ -0,0 +1,2 @@
+let count = 100; //int long => number
+let score = 9.6; //float double => number
---
+++ lesson8/lesson8TS.ts
@@ -0,0 +1,6 @@
+let test : number = 9.6;
+
+test = 999;
+
+let a : number; // let k yêu câù giá trị    
+const a1 : number = 12;// const thì có
