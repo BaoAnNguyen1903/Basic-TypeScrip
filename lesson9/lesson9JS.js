7fea7a7ba749987e33637c199e82013671b62e89
An Nguyen <anndb1903@gmail.com>
2025-03-31 13:43:29 +0700

    String Type

---
+++ lesson9/lesson9JS.js
@@ -0,0 +1,2 @@
+let name2 = 'bao an';
+let name1 = "baoan";
---
+++ lesson9/lesson9TS.ts
@@ -0,0 +1,5 @@
+let name3 : string = `bao 
+'an'`; // giấu \ \ để thoát ra dấu nháy đơn để view ra bao 'an'
+let name4 : string = `baoan ${name3}`;
+
+console.log(">>check name3: ", name4)
