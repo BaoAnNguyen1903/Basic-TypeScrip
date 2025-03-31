b7fe166b5bc9618a4955a9d5173550aa45ae4699
An Nguyen <anndb1903@gmail.com>
2025-03-31 14:16:56 +0700

    Tuple Types

---
+++ lesson13/lesson13TS.ts
@@ -0,0 +1,12 @@
+// let skills : (string | number)[] = ['bao an dep trai', 20]
+// skills.push('coding');
+// skills.push(69);
+
+
+//tuple: datatype / size / thu tu
+let skills2 : [string, number] = ['bao an dep trai', 20];
+
+// ?sau type goi la optional (co cx dc, k co cx dc) va` luu y phai o cuoi cung cua mang
+let skills3 : [boolean , string, number?]
+
+skills3 = [true, 'Baoan', 20]
